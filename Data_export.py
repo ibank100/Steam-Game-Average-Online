948b85de20fbd6574ed808779c10990fe31988de
Sinsawad Apichaisompol <43001220+ibank100@users.noreply.github.com>
2018-12-16 13:22:24 +0700

    Update Data_export.py

--- Data_export.py
+++ Data_export.py
@@ -1,19 +1,21 @@
-import csv
-import time
+import csv # <<< import for reading and writing data.
+import time # <<< import for delay time, makes it look more realistic.
 # ... is location file from your computer or notebook.
+
 def read():
+    ''' Open the csv file and then extract the data into the list. '''
     with open('.../Data/Data_Player_Averager.csv') as csvfile:
         print("Start Program.")
         print("Loading.")
         time.sleep(1)
         print("Reading data.")
-        reader = csv.reader(csvfile)
+        reader = csv.reader(csvfile) # <<< make data from csvfile to list but it's all data.
         pubg = []
         dota2 = []
         csgo = []
         poe = []
         rainbow6 = []
-        for row in reader:
+        for row in reader: # <<< make loop to extract the data into the list.
             pubg.append([row[0],row[1],row[2]])
             dota2.append([row[0],row[1],row[3]])
             csgo.append([row[0],row[1],row[4]])
@@ -36,6 +38,7 @@ def read():
         print("Finish Program.")
 
 def write(name, game):
+    ''' get infomation form read function and write to file csv. '''
     if name == 'pubg':
         writer = csv.writer(open(".../Data/PUBG.csv", 'w',  newline=''))
         for data in game:
